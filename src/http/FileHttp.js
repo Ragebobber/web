67909722ee77fe493a0f8133f86d4ca18672e2a9
Никита Калядин <stalknik98@gmail.com>
2023-08-16 15:46:34 +0300

    Add zip downloader

--- src/http/FileHttp.js
+++ src/http/FileHttp.js
@@ -4,3 +4,9 @@ export const addFile = async (data) => {
   const res = await $authHost.post("/api/file/admin/add", data);
   return res.data;
 };
+
+export const downloadClient = async () => {
+  return await $authHost.get("/api/file/downloads/client", {
+    responseType: "blob",
+  });
+};
